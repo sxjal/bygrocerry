1d9ba35c7a198fce16acd14c7db4fa47b99b2078
sajal <sajalsahu8@gmail.com>
2023-12-23 01:08:52 +0530

    built the base of the home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
-//import 'package:bygrocerry/adminpanel.dart';
-import 'package:bygrocerry/firebase_options.dart';
-import 'package:bygrocerry/pages/home/home.dart';
 import 'package:bygrocerry/pages/home/home_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter/services.dart';
+import 'package:bygrocerry/pages/home/home.dart';
 import 'package:bygrocerry/pages/login/components/login_auth_provider.dart';
 import 'package:bygrocerry/pages/provider/cart_provider.dart';
 import 'package:bygrocerry/pages/provider/favorite_provider.dart';
 import 'package:bygrocerry/pages/signup/components/signup_auth_provider.dart';
 import 'package:bygrocerry/pages/welcome/welcome_page.dart';
+import 'package:bygrocerry/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
+//import 'package:bygrocerry/pages/home/home_page.dart';
 
 void main() async {
   tz.initializeTimeZones();
@@ -58,15 +58,6 @@ class MyApp extends StatelessWidget {
               color: Colors.black,
             ),
           ),
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
           primarySwatch: Colors.blue,
         ),
         home: StreamBuilder(
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,13 +1,18 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:bygrocerry/appColors/app_colors.dart';
-import 'package:bygrocerry/model/user_model.dart';
 import 'package:bygrocerry/pages/detailPage/details_page.dart';
-import 'package:bygrocerry/route/routing_page.dart';
-import 'package:bygrocerry/widgets/build_drawer.dart';
-import 'package:bygrocerry/widgets/grid_view_widget.dart';
-import 'package:bygrocerry/widgets/single_product.dart';
+import 'package:bygrocerry/pages/welcome/welcome_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+// import 'package:bygrocerry/appColors/app_colors.dart';
+// import 'package:bygrocerry/pages/detailPage/details_page.dart';
+// import 'package:bygrocerry/route/routing_page.dart';
+// import 'package:bygrocerry/widgets/build_drawer.dart';
+// import 'package:bygrocerry/widgets/grid_view_widget.dart';
+// import 'package:bygrocerry/widgets/single_product.dart';
+import 'package:bygrocerry/model/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:stylish_bottom_bar/model/bar_items.dart';
+import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
 
 late UserModel userModel;
 Size? size;
@@ -54,8 +59,102 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
   }
 
+  dynamic selected;
+  var heart = false;
+  PageController controller = PageController();
+
   @override
-  Widget build(context) {
-    return Container();
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true, //to make floating action button notch transparent
+
+      //to avoid the floating action button overlapping behavior,
+      // when a soft keyboard is displayed
+      resizeToAvoidBottomInset: false,
+
+      bottomNavigationBar: StylishBottomBar(
+        backgroundColor: Colors.white,
+        option: AnimatedBarOptions(
+          // iconSize: 32,
+          barAnimation: BarAnimation.fade,
+          iconStyle: IconStyle.animated,
+          // opacity: 0.3,
+        ),
+        items: [
+          BottomBarItem(
+            icon: const Icon(
+              Icons.house_outlined,
+            ),
+            selectedIcon: const Icon(
+              Icons.house_rounded,
+            ),
+            selectedColor: Colors.green,
+            title: const Text('Home'),
+          ),
+          BottomBarItem(
+            icon: const Icon(
+              Icons.style_outlined,
+            ),
+            selectedIcon: const Icon(Icons.style),
+            selectedColor: Colors.green,
+            title: const Text('Favorites'),
+          ),
+          BottomBarItem(
+            icon: const Icon(
+              Icons.person_outline,
+            ),
+            selectedIcon: const Icon(
+              Icons.person,
+            ),
+            selectedColor: Colors.green,
+            title: const Text(
+              'Profile',
+            ),
+          ),
+        ],
+        hasNotch: true,
+        fabLocation: StylishBarFabLocation.end,
+        currentIndex: selected ?? 0,
+        onTap: (index) {
+          controller.jumpToPage(index);
+          setState(() {
+            selected = index;
+          });
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => WelcomePage(),
+              ),
+            );
+          });
+        },
+        backgroundColor: Colors.green,
+        child: Icon(
+          CupertinoIcons.cart,
+          color: Color.fromARGB(255, 255, 255, 255),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      body: SafeArea(
+        child: PageView(
+          controller: controller,
+          children: const [
+            Center(child: Text('Home')),
+            Center(child: Text('Style')),
+            Center(child: Text('Profile')),
+          ],
+        ),
+      ),
+    );
   }
 }
