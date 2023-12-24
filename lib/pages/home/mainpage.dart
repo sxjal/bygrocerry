93df6e45f14b3699eed530cf871744e12e6660e8
sajal <sajalsahu8@gmail.com>
2023-12-24 17:06:33 +0530

    adding customscrollbar

--- lib/pages/home/mainpage.dart
+++ lib/pages/home/mainpage.dart
@@ -55,7 +55,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 248, 248, 248),
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
       body: GestureDetector(
         onTap: () {
           FocusScope.of(context).unfocus();
@@ -197,7 +197,7 @@ class _HomePageState extends State<HomePage> {
                 top: MediaQuery.of(context).size.height * .3,
                 child: Container(
                   width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
+                  height: MediaQuery.of(context).size.height * .6,
                   decoration: BoxDecoration(
                     color: Color.fromARGB(255, 255, 255, 255),
                     borderRadius: BorderRadius.only(
@@ -229,4 +229,136 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     backgroundColor: Color.fromARGB(255, 248, 248, 248),
+  //     body: CustomScrollView(
+  //       // fit: StackFit.passthrough,
+  //       slivers: [
+  //         SliverAppBar(
+  //           expandedHeight: 250.0,
+  //           flexibleSpace: FlexibleSpaceBar(
+  //             title: Container(
+  //               decoration: BoxDecoration(
+  //                 gradient: LinearGradient(
+  //                   begin: Alignment.topLeft,
+  //                   end: Alignment.bottomRight,
+  //                   colors: [
+  //                     Color.fromRGBO(33, 160, 86, 1),
+  //                     Color.fromRGBO(64, 190, 117, 1)
+  //                   ],
+  //                 ),
+  //               ),
+  //               child: Column(
+  //                 mainAxisSize: MainAxisSize.min,
+  //                 children: [
+  //                   Row(
+  //                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //                     mainAxisSize: MainAxisSize.max,
+  //                     crossAxisAlignment: CrossAxisAlignment.center,
+  //                     children: [
+  //                       Column(
+  //                         crossAxisAlignment: CrossAxisAlignment.start,
+  //                         children: [
+  //                           Text(
+  //                             "Welcome,",
+  //                             textAlign: TextAlign.left,
+  //                             style: GoogleFonts.montserrat(
+  //                               fontSize: 14,
+  //                               fontWeight: FontWeight.w800,
+  //                               color: Color.fromARGB(255, 225, 225, 225),
+  //                             ),
+  //                           ),
+  //                           SizedBox(
+  //                             height: MediaQuery.of(context).size.height * .01,
+  //                           ),
+  //                           Text(
+  //                             widget.user.fullName,
+  //                             style: GoogleFonts.montserrat(
+  //                               fontSize: 16,
+  //                               fontWeight: FontWeight.bold,
+  //                               color: Color.fromARGB(255, 255, 255, 255),
+  //                             ),
+  //                           ),
+  //                         ],
+  //                       ),
+  //                       Spacer(),
+  //                       GestureDetector(
+  //                         onTap: () {
+  //                           Navigator.of(context).push(
+  //                             MaterialPageRoute(
+  //                               builder: (context) => ProfilePage(),
+  //                             ),
+  //                           );
+  //                         },
+  //                         child: Container(
+  //                           height: MediaQuery.of(context).size.height * .1,
+  //                           width: MediaQuery.of(context).size.height * .1,
+  //                           decoration: BoxDecoration(
+  //                             borderRadius: BorderRadius.circular(50),
+  //                             image: widget.user.imageurl == "notset"
+  //                                 ? DecorationImage(
+  //                                     image: buildUserImage(
+  //                                       "images/user.png",
+  //                                     ), // AssetImage("images/user.png"),
+  //                                     fit: BoxFit.cover,
+  //                                   )
+  //                                 : DecorationImage(
+  //                                     image: buildImage(
+  //                                       widget.user.imageurl,
+  //                                     ), // NetworkImage(widget.user.imageurl),
+  //                                     fit: BoxFit.cover,
+  //                                   ),
+  //                           ),
+  //                         ),
+  //                       ),
+  //                     ],
+  //                   ),
+  //                   SizedBox(
+  //                     height: MediaQuery.of(context).size.height * .03,
+  //                   ),
+  //                 ],
+  //               ),
+  //             ),
+  //             background: Image.asset(
+  //               "images/bgpattern.png",
+  //               fit: BoxFit.fill,
+  //             ),
+  //           ),
+  //           bottom: AppBar(
+  //             title: Container(
+  //               height: 40,
+  //               child: TextFormField(
+  //                 onChanged: (value) {
+  //                   setState(() {
+  //                     query = value;
+  //                   });
+  //                 },
+  //                 decoration: InputDecoration(
+  //                   suffixIcon: Icon(Icons.search),
+  //                   fillColor: Color.fromARGB(139, 251, 251, 251),
+  //                   hintText: "Search by name...",
+  //                   hintStyle: GoogleFonts.montserrat(
+  //                     fontSize: 12,
+  //                     fontWeight: FontWeight.w500,
+  //                     color: Color.fromARGB(159, 0, 0, 0),
+  //                   ),
+  //                   suffixIconColor: Color.fromARGB(114, 0, 0, 0),
+  //                   focusColor: Color.fromARGB(114, 0, 0, 0),
+  //                   filled: true,
+  //                   border: OutlineInputBorder(
+  //                     borderRadius: BorderRadius.circular(10),
+  //                     borderSide: BorderSide.none,
+  //                   ),
+  //                 ),
+  //                 cursorColor: Color.fromARGB(114, 0, 0, 0),
+  //               ),
+  //             ),
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 }
