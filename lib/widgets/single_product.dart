1006bf850a1243158f64056e93e5196b3494fbd5
sajal <sajalsahu8@gmail.com>
2023-12-25 16:31:53 +0530

    favs thing

--- lib/widgets/single_product.dart
+++ lib/widgets/single_product.dart
@@ -202,103 +202,13 @@ class _SingleProductState extends State<SingleProduct> {
                     fontSize: 10,
                   ),
                 ),
-                // LayoutBuilder(
-                //   builder: (BuildContext context, BoxConstraints constraints) {
-                //     final span = TextSpan(
-                //       text:
-                //           "hahabsdkjasbdfjhawebfjhebwrhfjkaberfhjberosadsadsadsfuhberouyfberuyfberuoyfberuoyfberyuofberouyfberuybf", // widget.productDescription,
-                //       style: GoogleFonts.poppins(
-                //         fontWeight: FontWeight.w400,
-                //         fontSize: 10,
-                //       ),
-                //     );
-                //     final tp = TextPainter(
-                //       text: span,
-                //       textDirection: TextDirection.ltr,
-                //       maxLines: 2,
-                //     );
-                //     tp.layout(maxWidth: constraints.maxWidth);
-
-                //     if (tp.didExceedMaxLines) {
-                //       // The text is overflowing
-                //       overflowing = true;
-                //     } else {
-                //       // The text is not overflowing
-                //       overflowing = false;
-                //     }
-
-                //     return Text.rich(
-                //       span,
-                //       maxLines: overflowing && readmore ? 10 : 2,
-                //       softWrap: true,
-                //       overflow: TextOverflow.ellipsis,
-                //     );
-                //   },
-                // ),
-                // TextButton(
-                //   onPressed: () {},
-                //   child: Text("Ream more..."),
-                // ),
               ],
             ),
           ),
           SizedBox(
             width: MediaQuery.of(context).size.width * 0.001,
           ),
-          //contains the card icon on the top and favorite on the bottom
-          // Column(
-          //   children: [
-          //     GestureDetector(
-          //       onTap: () async {
-          //         DocumentReference docRef = FirebaseFirestore.instance
-          //             .collection("cart")
-          //             .doc(FirebaseAuth.instance.currentUser!.uid)
-          //             .collection("userCart")
-          //             .doc(widget.productId);
 
-          //         DocumentSnapshot docSnap = await docRef.get();
-          //         if (docSnap.exists) {
-          //           print('Product is already in the cart');
-          //         } else {
-          //           cartAdded = true;
-          //           docRef.set(
-          //             {
-          //               "productId": widget.productId,
-          //               "productImage": widget.productImage,
-          //               "productName": widget.productName,
-          //               "productPrice": widget.productPrice,
-          //               "productOldPrice": widget.productPrice,
-          //               "productDescription": widget.productDescription,
-          //               "productQuantity": 1,
-          //               "productCategory": widget.productCategory,
-          //             },
-          //           );
-          //         }
-          //         RoutingPage.goTonext(
-          //           context: context,
-          //           navigateTo: CheckOutPage(),
-          //         );
-          //       },
-          //       child: Container(
-          //         padding: EdgeInsets.all(10),
-          //         //   width: MediaQuery.of(context).size.width * .15,
-          //         //  height: MediaQuery.of(context).size.width * .15,
-          //         decoration: BoxDecoration(
-          //           color: Color.fromRGBO(254, 205, 67, 1),
-          //           borderRadius: BorderRadius.circular(50),
-          //         ),
-          //         child: Icon(
-          //           cartAdded
-          //               ? CupertinoIcons.cart_fill
-          //               : CupertinoIcons.cart,
-          //           size: 14,
-          //           color: const Color.fromARGB(255, 0, 0, 0),
-          //         ),
-          //       ),
-          //     ),
-          //     SizedBox(
-          //       height: MediaQuery.of(context).size.width * .05,
-          //     ),
           //     Container(
           //       width: MediaQuery.of(context).size.width * .08,
           //       height: MediaQuery.of(context).size.width * .08,
@@ -327,6 +237,38 @@ class _SingleProductState extends State<SingleProduct> {
                       image: NetworkImage(widget.productImage),
                     ),
                     borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                Positioned(
+                  right: MediaQuery.of(context).size.width * .070,
+                  top: MediaQuery.of(context).size.width * .02,
+                  child: GestureDetector(
+                    onTap: () {
+                      print("clicked on favs");
+                      setState(
+                        () {
+                          if (isFavorite == true) {
+                            favoriteProvider.favorite(
+                              productId: widget.productId,
+                              productCategory: widget.productCategory,
+                              productRate: widget.productRate,
+                              productOldPrice:
+                                  double.parse(widget.productOldPrice),
+                              productPrice: double.parse(widget.productPrice),
+                              productImage: buildimage,
+                              productFavorite: true,
+                              productName: widget.productName,
+                            );
+                          } else if (isFavorite == false) {
+                            favoriteProvider.deleteFavorite(
+                                productId: widget.productId);
+                          }
+                        },
+                      );
+                    },
+                    child: Icon(
+                      Icons.favorite,
+                    ),
                   ),
                 ),
                 Positioned(
@@ -475,30 +417,6 @@ class _SingleProductState extends State<SingleProduct> {
           ),
         ],
       ),
-
-      //           onPressed: () {
-      //             setState(
-      //               () {
-      //                 //  isFavorite = !isFavorite;
-
-      //                 if (isFavorite == true) {
-      //                   favoriteProvider.favorite(
-      //                     productId: widget.productId,
-      //                     productCategory: widget.productCategory,
-      //                     productRate: widget.productRate,
-      //                     productOldPrice: double.parse(widget.productOldPrice),
-      //                     productPrice: double.parse(widget.productPrice),
-      //                     productImage: buildimage,
-      //                     productFavorite: true,
-      //                     productName: widget.productName,
-      //                   );
-      //                 } else if (isFavorite == false) {
-      //                   favoriteProvider.deleteFavorite(
-      //                       productId: widget.productId);
-      //                 }
-      //               },
-      //             );
-      //           },
     );
   }
 }
